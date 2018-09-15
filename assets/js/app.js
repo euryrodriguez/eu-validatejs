1df0975246020916d34bb6c9b528f7326540bcb1
eury <eury958@gmail.com>
2018-09-15 17:25:31 -0400

    Change: Submit form after validation.

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,12 @@ let app = new App();
 
 $(document).on('click', '#submit', function (e) {
     let selector = $(this),
-        form = selector.closest('form');
-    console.log(app.validate.validateForm(form));
+        form = selector.closest('form'),
+        data = form.serializeArray();
+    if (app.validate.validateForm(form)) {
+        console.log(data);
+        form.submit();
+    } else {
+        console.log('No envio');
+    }
 });
